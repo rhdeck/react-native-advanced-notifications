82e8e36afd98505a182f71ffe3f0f64e34812e3c
Ray Deck <ray@raydeck.com>
2020-04-07 18:49:32 -0500

    Updating to RNA nomenclature

--- ReactNativeAdvancedNotifications.podspec
+++ ReactNativeAdvancedNotifications.podspec
@@ -1,16 +1,16 @@
 require 'json'
 package = JSON.parse(File.read(File.join(__dir__, 'package.json')))
 Pod::Spec.new do |s|
-  s.name         = "ReactNativeIosNotificationsSwift"
+  s.name         = "ReactNativeAdvancedNotifications"
   s.version      = package['version']
   s.summary      = package['description']
   s.authors      = "Ray Deck"
   s.homepage     = package['homepage']
   s.license      = package['license']
   s.platform     = :ios, "9.0"
-  s.module_name  = 'ReactNativeIosNotificationsSwift'
-  s.source       = { :git => "https://github.com/rhdeck/react-native-ios-notifications-swift.git", :tag => "#{s.version}" }
+  s.module_name  = 'ReactNativeAdvancedNotifications'
+  s.source       = { :git => "https://github.com/rhdeck/react-native-advanced-notifications-swift.git", :tag => "#{s.version}" }
   s.source_files  = "ios/**/*.{h,m,swift}"
   s.dependency 'React'
-  s.dependency 'ReactNativeSwiftRegistry'
+  s.dependency 'ReactNativeAdvancedRegistry'
 end
--- ios/rnins/rnins.swift
+++ ios/rnins/rnins.swift
@@ -1,6 +1,29 @@
 import Foundation
-import ReactNativeSwiftRegistry
-
+import UserNotifications
+import ReactNativeAdvancedRegistry
+@available(iOS 10.0, *)
+@objc
+class RNNotificationDelegate: NSObject, UNUserNotificationCenterDelegate, RNSStartable {
+    static var instance = RNNotificationDelegate();
+    
+    static func runOnStart(_ application: UIApplication) {
+        UNUserNotificationCenter.current().delegate = RNNotificationDelegate.instance
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, openSettingsFor notification: UNNotification?) {
+        let _ = RNSMainRegistry.triggerEvent(type: "app.didOpenSettingsForNotification", data: notification as Any)
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        RNSMainRegistry.setData(key: "notification.payload", value: response.notification.request.content.userInfo)
+        let _ = RNSMainRegistry.triggerEvent(type: "app.didReceiveResponse", data: ["response": response, "completionHandler": completionHandler])
+        
+    }
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        if(  !RNSMainRegistry.triggerEvent(type: "app.willPresentNotification", data: ["notification": notification, "completionHandler": completionHandler])) {
+            completionHandler(.alert)
+        }
+        
+    }
+}
 //Note that for objective-c (and therefore RN) to see the class you need to give the @objc hint
 //Also, any method exposed to objective-c runtime will also require the hint.
 @objc(rnins)
@@ -19,7 +42,7 @@ class rnins: NSObject {
         }
         let _ = RNSMainRegistry.addEvent(type: "app.didReceiveRemoteNotification", key: "RNINS") { data in
             guard let n = data as? [String: Any?] else { return false }
-            RNCPushNotificationIOS.didReceiveRemoteNotification(n)
+            RNCPushNotificationIOS.didReceiveRemoteNotification(n as [AnyHashable : Any])
             return true
         }
         let _ = RNSMainRegistry.addEvent(type: "app.didFailToRegisterForRemoteNotifications", key: "RNINS") { data in
