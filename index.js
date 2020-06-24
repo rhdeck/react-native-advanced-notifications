9cdbf3199f5894c5ead0c0c2316f65f0a308c012
Ray Deck <ray@raydeck.com>
2020-06-24 15:05:02 -0500

    Adding initial notificationaction to the useNotificationAction hook

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { getData, removeData, addEvent } from "@react-native-advanced/core";
 import { Platform } from "react-native";
 import Deferred from "es6-deferred";
+import useAsyncEffect from "@raydeck/useasynceffect";
 let notification;
 let waiters = [];
 if (Platform.OS === "react-native") {
@@ -32,7 +33,15 @@ addEvent("notificationAction", "counselor", (data) => {
 });
 
 const useNotificationAction = () => {
-  const [notificationAction, setNotificationAction] = useState();
+  const [notificationAction, setNotificationAction] = useState(
+    notificationAction
+  );
+  useAsyncEffect(async () => {
+    const n = await getInitialNotificationAction();
+    if (n) {
+      setNotificationAction(n);
+    }
+  }, []);
   useEffect(() => {
     notificationActionListeners.push(setNotificationAction);
     return () => {
