7406d887846affc485085dd3ac4670366dea803b
Ray Deck <ray@raydeck.com>
2020-06-24 14:59:32 -0500

    Adding code to listen for notificationactions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-import { getData, removeData } from "@react-native-advanced/core";
+import { useState, useEffect } from "react";
+import { getData, removeData, addEvent } from "@react-native-advanced/core";
 import { Platform } from "react-native";
 import Deferred from "es6-deferred";
 let notification;
@@ -18,10 +19,28 @@ if (Platform.OS === "react-native") {
 } else {
   notification = null;
 }
-const getInitialNotification = async () => {
+const getInitialNotificationAction = async () => {
   if (typeof notification !== "undefined") return notification;
   const { promise, resolve } = new Deferred();
   waiters.push(resolve);
   return await promise;
 };
-export { getInitialNotification };
+let notificationActionListeners = [];
+addEvent("notificationAction", "counselor", (data) => {
+  notificationActionListeners.map((listener) => listener(data));
+  console.log("Got callback from notificationAction", data);
+});
+
+const useNotificationAction = () => {
+  const [notificationAction, setNotificationAction] = useState();
+  useEffect(() => {
+    notificationActionListeners.push(setNotificationAction);
+    return () => {
+      notifcationActionListeners = notificationActionListeners.filter(
+        (listener) => listener !== setNotificationAction
+      );
+    };
+  }, []);
+  return notificationAction;
+};
+export { getInitialNotificationAction, useNotificationAction };
